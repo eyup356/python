34671b45c077e2e1ac8ca8e95cee1d5e0a0a252d
Eyüp Türkaslan <30681679+eyup356@users.noreply.github.com>
2023-09-01 17:18:35 +0300

    Delete TextToSpeech.py

--- TextToSpeech.py
+++
@@ -1,7 +0,0 @@
-from gtts import gTTS
-import os
-text = input("Please input any words (Turkish): ")
-language = "tr"
-speech = gTTS(text=text,lang=language,slow=False)
-speech.save("text.mp3")
-os.system("start text.mp3")
